4280fbe326bcd0f1a1a37b0a9c48fd9bb4422e76
Pratyush <pandapratyush005@gmail.com>
2025-02-28 18:59:34 +0530

    final production

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React from "react"
-import Aurora from './Aurora.jsx';
-import GradientText from './Gradient.jsx'
-import { useEffect,useState,useRef } from "react";
+import React from "react";
+import Aurora from "./Aurora.jsx";
+import GradientText from "./Gradient.jsx";
+import { useEffect, useState, useRef } from "react";
 import { gsap } from "gsap";
 function App() {
   const [path, setPath] = useState("M 10 100 Q 500 100 990 100");
@@ -45,44 +45,45 @@ function App() {
   }, []);
   return (
     <main className="h-[100vh] bg-black">
-    <div className="absolute b pointer-events-auto w-[100vw] h-[320px]">
-
-      <Aurora
-     colorStops={["#71706C", "#1F1F1F", "#858584"]}
-     blend={0.5}
-     amplitude={1.0}
-     speed={0.5}
-   /> 
-    </div>
-      <section className='relative z-10 h-[100vh] w-[100vw] p-0 m-0 overflow-hidden flex justify-center
-      items-center flex-col'>  
-<h1 className="text-white text-[32px] lg:text-[56px] font-rethinkSans flex gap-3"><span className=" font-[700] italic">GSAP</span>
-<GradientText
-  colors={["#71706C", "#FFFFFF", "#858584", "#FFFFFF", "#858584"]}
-  animationSpeed={3}
-  showBorder={false}
-  className="font-[200]"
->
-  SVG animation
-</GradientText>
-</h1>
-     <div ref={stringRef} className="h-[200px] flex justify-center items-center w-[100vw]">
-      <svg className="w-[80vw] h-auto"
-        viewBox="0 0 1000 200"
-        preserveAspectRatio="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d={path}
-          stroke="white"
-          stroke-width="1"
-          fill="transparent"
+      <div className="absolute b pointer-events-auto w-[100vw] h-[400px]">
+        <Aurora
+          colorStops={["#71706C", "#1F1F1F", "#858584"]}
+          blend={0.5}
+          amplitude={1.0}
+          speed={0.5}
         />
-      </svg>
-    </div>
+      </div>
+      <section
+        className="relative z-10 h-[100vh] w-[100vw] p-0 m-0 overflow-hidden flex justify-center
+      items-center flex-col"
+      >
+        <h1 className="text-white text-[32px] lg:text-[56px] font-rethinkSans flex gap-3">
+          <span className=" font-[700] italic">GSAP</span>
+          <GradientText
+            colors={["#71706C", "#FFFFFF", "#858584", "#FFFFFF", "#858584"]}
+            animationSpeed={3}
+            showBorder={false}
+            className="font-[200]"
+          >
+            SVG animation
+          </GradientText>
+        </h1>
+        <div
+          ref={stringRef}
+          className="h-[200px] flex justify-center items-center w-[100vw]"
+        >
+          <svg
+            className="w-[80vw] h-auto"
+            viewBox="0 0 1000 200"
+            preserveAspectRatio="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d={path} stroke="white" stroke-width="1" fill="transparent" />
+          </svg>
+        </div>
       </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
